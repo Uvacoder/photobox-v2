0b0ee1fd255caf24701a98846ee44ff8ca88ea8c
Andrii Protsenko <aprotsenko@liveperson.com>
2022-02-05 09:20:30 +0200

    Add typings

---
+++ src/typings/sticky.d.ts
@@ -0,0 +1,13 @@
+//export namespace defaultd{
+
+//}
+
+//declare namespace sticky{
+
+declare module "sticky-js" {
+    export default class Sticky {
+        constructor(selector: string);
+    }
+}
+
+//}
